e821c77a9d5deb6b224a27d15e2fe13d67741f42
Latjo <tweinsjo@gmail.com>
2018-07-04 13:56:51 +0200

    upload

--- plot1.R
+++ plot1.R
@@ -1,8 +1,7 @@
 ## Create plot1
 
 # Read data into a table
-EL <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", header= TRUE, sep=";",
-                 stringsAsFactors = FALSE)
+EL <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", header = TRUE, sep =";", stringsAsFactors = FALSE)
 
 # Load dplyr package
 library(dplyr)
@@ -13,15 +12,25 @@ EL <- filter(EL, Date=="1/2/2007"|Date=="2/2/2007")
 # Load lubdridate package
 library(lubridate)
 
-# Convert variables to appropriate classes
+# Convert EL$Date to date class
 EL$Date <- dmy(EL$Date)
-# EL$Time <- hms(EL$Time)
+
+# Add a new column which holds both date and time.
+EL <- mutate(EL, DateTime = as.POSIXct(strptime(paste(EL$Date, EL$Time), format = "%Y-%m-%d %H:%M:%S")))
+
+# Make the newly created column DateTime become first column.
+EL <- EL[,c(10,1:9)]
+
+# Convert variables to appropriate classes
 EL$Global_active_power <- as.numeric(EL$Global_active_power)
 EL$Global_reactive_power <- as.numeric(EL$Global_reactive_power)
 EL$Voltage <- as.numeric(EL$Voltage)
 EL$Global_intensity <- as.numeric(EL$Global_intensity)
 EL$Sub_metering_1 <- as.numeric(EL$Sub_metering_1)
 EL$Sub_metering_2 <- as.numeric(EL$Sub_metering_2)
-# EL$Sub_metering_3 <- as.numeric(EL$Sub_metering_3)
+EL$Sub_metering_3 <- as.numeric(EL$Sub_metering_3)
 
+# Create and output diagram to png file "plot1.png"
+png(file = "plot1.png")
 hist(EL$Global_active_power, main= "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+dev.off()
